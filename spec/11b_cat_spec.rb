f629f30877cce732e069867b47408aa43d22165c
Matson Ovstedal <matsonov@gmail.com>
2022-06-01 12:39:36 -0700

    Complete 11b_cat_spec

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:tunami) { described_class.new('tunami', 'tabby', 'burl') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'responds to #talk' do
+    include_examples 'shared method name'
+    # expect(tunami).to respond_to(:talk).with(1).argument
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(tunami).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(tunami).to be_hiding
   end
 end
